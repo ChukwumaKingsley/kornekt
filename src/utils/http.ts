b96088f07557cd2039b0ad5a3d2e82a2aee23bd2
Chukwuma Kingsley Chidubem <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-10-29 10:31:05 +0100

    Auth problems

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.headers.common["x-auth-token"] = `Bearer ${localStorage.getItem("accessToken") || ""}`;
+axios.defaults.headers.common["x-auth-token"] = `Bearer ${localStorage.getItem("accessToken") || ""}`;
 axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem("accessToken") || ""}`;
 
