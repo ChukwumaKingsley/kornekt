387cf1628fcf5e1b8e76da5843153abc18db5a3d
Chukwuma Kingsley Chidubem <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-10-29 08:48:23 +0100

    Auth problems

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-axios.defaults.headers.common["x-auth-token"] = `Bearer ${localStorage.getItem("accessToken") ?? ""}`;
+axios.defaults.headers.common["x-auth-token"] = `Bearer ${localStorage.getItem("accessToken") || ""}`;
 axios.defaults.headers.common["Content-Type"] = "application/json";
-axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem("accessToken") ?? " "}`;
+axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem("accessToken") || ""}`;
 
 const http = axios.create({
-	baseURL: "https://fastapi-2.onrender.com",
-	timeout: 10000,
+  baseURL: "https://fastapi-2.onrender.com",
+  timeout: 10000,
 });
 
 export default http;
-// https://fastapi-2.onrender.com
