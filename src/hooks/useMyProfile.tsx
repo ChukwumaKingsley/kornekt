20f28753a0bd0bdc92e36e7620efa573c4391597
Chukwuma Kingsley Chidubem <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-10-29 10:51:13 +0100

    Auth problems

--- src/hooks/useMyProfile.tsx
+++ src/hooks/useMyProfile.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import http from "../utils/http";
 import { useToast } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 export interface UserProfile {
   email: string;
@@ -15,7 +15,7 @@ export interface UserProfile {
 
 function useMyProfile() {
   const toast = useToast();
-  const navigate =  useNavigate()
+  // const navigate =  useNavigate()
   
   
   return useQuery<UserProfile, Error>({
