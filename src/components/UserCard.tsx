9ab44752826be36dd3be5fb12190ef64364d8bf6
Chukwuma Kingsley Chidubem <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-10-29 12:26:15 +0100

    Left align names and email in usercard

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -6,7 +6,7 @@ const UserCard = (props: any) => {
   return (
           <Flex as={NavLink} to={`/home/user/${props.user_id}`} maxWidth={'400px'}  width={"90%"} ml='20px' mb={'20px'} alignSelf={'center'} alignItems={'center'} borderBottom={'1px'} borderColor={'gray.600'} p={'10px'}>
             <Avatar size={'md'} marginRight={'5px'} bg='blue.900' bgSize={'inherit'} src={'hll'} name={props.user_name} />
-            <VStack ml={'10px'}>
+            <VStack ml={'10px'} textAlign={'left'}>
               <Text fontSize="sm" color="gray.500" mb={'1px'}>
                 {props.user_name}
               </Text>
