deefdad5e2b90cb61148f9500d5aeca072306616
Chukwuma Kingsley Chidubem <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-10-30 13:10:10 +0100

    Increased max width of UserCard

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const UserCard = (props: any) => {
 
   return (
-          <Flex as={NavLink} to={`/home/user/${props.user_id}`} maxWidth={'400px'}  width={"90%"} ml='20px' mb={'20px'} alignSelf={'center'} alignItems={'center'} borderBottom={'1px'} borderColor={'gray.600'} p={'10px'}>
+          <Flex as={NavLink} to={`/home/user/${props.user_id}`} maxWidth={'800px'}  width={"90%"} ml='20px' mb={'20px'} alignSelf={'center'} alignItems={'center'} borderBottom={'1px'} borderColor={'gray.600'} p={'10px'}>
             <Avatar size={'md'} marginRight={'5px'} bg='blue.900' bgSize={'inherit'} src={'hll'} name={props.user_name} />
             <Flex ml={'10px'} flexDir={'column'}>
               <Text fontSize="sm" color="gray.500" mb={'1px'} textAlign={'left'}>
