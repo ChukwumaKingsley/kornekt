7a1e1c5e97b1bc2fab75c988d9b796e01a11f7f9
Chukwuma Kingsley Chidubem <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-11-06 10:08:14 +0100

    Changed to production server

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -5,9 +5,9 @@ axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem("accessToken") ?? ""}`;
 
 const http = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  // baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://fastapi-2.onrender.com",
   timeout: 10000,
 });
 
 export default http;
-// https://fastapi-2.onrender.com
